c845d9f301a08f9b05d95065d7dd0f21843923aa
ibabyblue <zhangbiao@joyme.sg>
2024-08-13 17:27:21 +0800

    is-增加UserDefaults使用方式

--- iSwift.playground/Pages/08-属性.xcplaygroundpage/Contents.swift
+++ iSwift.playground/Pages/08-属性.xcplaygroundpage/Contents.swift
@@ -285,4 +285,13 @@ final public class FileManager {
     private init() {}
 }
 
+//MARK: - UserDefaults
+enum UDKey {
+    static let k1 = "token"
+}
+let ud = UserDefaults.standard
+ud.set("xxxxxx", forKey: UDKey.k1)
+let tk = ud.string(forKey: UDKey.k1)
+print(tk ?? "")
+
 //: [Next](@next)
