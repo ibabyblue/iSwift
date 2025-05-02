e3b3fd9c5626fbb4305c09e10e818f0ee1df7d1d
ibabyblue <ibabyblue_z@icloud.com>
2025-05-02 14:36:38 +0800

    is-修正错别字

--- iSwift.playground/Pages/30-面试题.xcplaygroundpage/Contents.swift
+++ iSwift.playground/Pages/30-面试题.xcplaygroundpage/Contents.swift
@@ -35,7 +35,7 @@ var greeting = "Hello, playground"
 //MARK: 2、Swift中的Struct和Class的区别？
 /*
  1、Struct是值类型、Class是引用类型
- 2、Struct - 节省新能，不能被继承
+ 2、Struct - 节省性能，不能被继承
  3、struct ⾥的 class，即使 struct 复制了，它也是引⽤类型 ； 重要：例外，闭包⾥使⽤ Struct 是⼀份引⽤，除⾮明确的 copy ⼀份
  */
 
